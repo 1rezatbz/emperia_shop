e9944f49dd84aef57c88a98aed88bd63ea87886e
reza <1rezartabriz@gmail.com>
2023-04-25 12:50:34 +0100

    feat: add check_db function to create or check for existing HTML database
    The check_db function checks if the HTML file exists at DB_PATH and creates it if it doesn't. If the file already exists, it checks if it contains a table element and adds one if it doesn't.

---
+++ src/config.py
@@ -0,0 +1,23 @@
+import os
+from bs4 import BeautifulSoup
+
+# Path to the HTML file to be used as a database
+DB_PATH = "../Emperia/index.html"
+
+
+# Function to check if the database file exists and create it if it does not
+def check_db() -> None:
+    # Check if the database file exists
+    if os.path.exists(DB_PATH):
+        print(f"db '{DB_PATH}' already exists")
+        with open(DB_PATH, "r") as f:
+            content = f.read()
+        if BeautifulSoup(content, "html.parser").find("table") == None:
+            # If the file does not contain a table element, add one to the file
+            with open(DB_PATH, "w") as f:
+                f.write("<table><tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th></tr></table>")
+    else:
+        # If the file does not exist, create it and add a table element to it
+        with open(DB_PATH, "w") as f:
+            f.write("<table><tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th></tr></table>")
+        print(f"db '{DB_PATH}' created")
